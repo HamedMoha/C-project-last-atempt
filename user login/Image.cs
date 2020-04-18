311d3711fa6eda3f02c8c56fc411276885e21bc5
HAMED HAMED SAEGH <1742754@stu.kau.edu.sa>
2020-04-18 18:10:26 +0300

    Add project files.

---
+++ user login/Image.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace user_login
+{
+    public partial class Image : Form
+    {
+        public Image()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadButt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                pictureBox1.Image = Image.FromFile("C:\\Users\\USER\\Desktop\\C#\\20.png");
+            }
+            catch
+            {
+                MessageBox.Show("Image file not found!");
+            }
+
+        }
+
+       
+    }
+}
+ 
